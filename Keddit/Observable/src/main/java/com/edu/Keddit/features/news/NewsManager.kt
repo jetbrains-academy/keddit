619d396ef8efbe9e0b81eaabee238b6d8118dc91
Geravant <igor.gera.gerasim@gmail.com>
2018-09-12 12:17:41 +0300

    Task 21 (Observable) anwer placeholders rearranged, code for lambda in getNews in NewsManager.kt deleted.

--- Keddit/Observable/src/main/java/com/edu/Keddit/features/news/NewsManager.kt
+++ Keddit/Observable/src/main/java/com/edu/Keddit/features/news/NewsManager.kt
@@ -12,19 +12,7 @@ class NewsManager() {
 
     fun getNews(): Observable<List<RedditNewsItem>> {
         return Observable.create {
-            subscriber ->
-            val news = mutableListOf<RedditNewsItem>()
-            for (i in 1..10) {
-                news.add(RedditNewsItem(
-                        "author$i",
-                        "Title $i",
-                        i, // number of comments
-                        1457207701L - i * 200, // time
-                        "http://lorempixel.com/200/200/technics/$i", // image url
-                        "url"
-                ))
-            }
-            subscriber.onNext(news)
+           //Will be completed in the nest task
         }
     }
 }
