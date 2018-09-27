9f0efe21ab1b14fa57154e83c9010dd6128d9303
Geravant <igor.gera.gerasim@gmail.com>
2018-09-27 13:48:39 +0300

    Task 11 (Extension functions) placeholder in the Extensions.kt is removed, task description modified accordingly.

--- Keddit/Extension Functions/src/main/java/com/edu/Keddit/features/news/NewsFragment.kt
+++ Keddit/Extension Functions/src/main/java/com/edu/Keddit/features/news/NewsFragment.kt
@@ -18,7 +18,7 @@ class NewsFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.news_fragment, container, false)
+        //val view = inflater.inflate(R.layout.news_fragment, container, false)
         return container?.inflate(R.layout.news_fragment)
     }
 
