d194fe829f948b574f7af4af24728d3d915a0834
Geravant <igor.gera.gerasim@gmail.com>
2018-10-04 15:29:56 +0300

    Task 18 (Filter) tests implemented.

---
+++ Keddit/Filter/src/test/java/com/edu/Keddit/SolutionTest.kt
@@ -0,0 +1,60 @@
+package com.edu.Keddit
+
+import android.support.v7.widget.RecyclerView
+import com.edu.Keddit.api.adapter.test.NewsAdapter
+import com.edu.Keddit.commons.RedditNewsItem
+import com.edu.Keddit.commons.adapter.AdapterConstants
+import com.edu.Keddit.commons.adapter.ViewType
+import com.edu.Keddit.features.news.NewsFragment
+import kotlinx.android.synthetic.main.activity_main.view.*
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.robolectric.Robolectric
+import kotlinx.android.synthetic.main.news_fragment.*
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.util.ReflectionHelpers
+
+@RunWith(RobolectricTestRunner::class)
+class SolutionTest {
+    private var activity: MainActivity? = null
+    @Before
+    fun setup() {
+        activity = Robolectric.buildActivity(MainActivity::class.java).create().visible().start().get()
+    }
+    @Test
+    fun testThereAre10BeforeAdd() {
+        val actualResult  = (activity?.news_list?.adapter as NewsAdapter).getNews().size
+        Assert.assertEquals(10, actualResult)
+    }
+    @Test
+    fun testThereAre11AfterAddNEWS() {
+        val newsToAdd = mutableListOf<RedditNewsItem>()
+        newsToAdd.add(RedditNewsItem(
+                    "author",
+                    "Title ",
+                    0, // number of comments
+                    1457207701L - 12 * 200, // time
+                    "http://lorempixel.com/200/200/technics/$", // image url
+                    "url"
+            ))
+        (activity?.news_list?.adapter as NewsAdapter).addNews(newsToAdd)
+        val items = ReflectionHelpers.getField<ArrayList<ViewType>>(activity?.news_list?.adapter, "items")
+        val actualResult  = (activity?.news_list?.adapter as NewsAdapter).getNews().size
+        Assert.assertEquals(items.size-1, actualResult)
+    }
+    @Test
+    fun testThereAre10AfterAddNotNEWS() {
+        val newsToAdd = mutableListOf<RedditNewsItem>()
+        val notNews = object : ViewType {
+            override fun getViewType() = AdapterConstants.LOADING
+        }
+        val items = ReflectionHelpers.getField<ArrayList<ViewType>>(activity?.news_list?.adapter, "items")
+        items.add(notNews)
+        (activity?.news_list?.adapter as NewsAdapter).addNews(newsToAdd)
+        val actualResult  = (activity?.news_list?.adapter as NewsAdapter).getNews().size
+        Assert.assertNotEquals(actualResult, items.size-1)
+    }
+
+}
