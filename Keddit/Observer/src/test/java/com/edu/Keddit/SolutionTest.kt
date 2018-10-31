c27b6de782481559492099a5aa9da2b6c429fab9
Geravant <igor.gera.gerasim@gmail.com>
2018-10-31 19:02:56 +0300

    Task 23 Observer test added.

---
+++ Keddit/Observer/src/test/java/com/edu/Keddit/SolutionTest.kt
@@ -0,0 +1,46 @@
+package com.edu.Keddit
+
+import android.support.v7.widget.RecyclerView
+import com.edu.Keddit.api.adapter.test.NewsAdapter
+import com.edu.Keddit.commons.RedditNewsItem
+import com.edu.Keddit.commons.adapter.AdapterConstants
+import com.edu.Keddit.commons.adapter.ViewType
+import com.edu.Keddit.features.news.NewsFragment
+import com.edu.Keddit.features.news.NewsManager
+import kotlinx.android.synthetic.main.activity_main.view.*
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.robolectric.Robolectric
+import kotlinx.android.synthetic.main.news_fragment.*
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.util.ReflectionHelpers
+
+@RunWith(RobolectricTestRunner::class)
+class SolutionTest {
+    private var activity: MainActivity? = null
+    private var newsFragment: NewsFragment? = null
+    @Before
+    fun setup() {
+        activity = Robolectric.buildActivity(MainActivity::class.java).create().visible().start().get()
+        newsFragment = activity?.supportFragmentManager!!.fragments[0] as NewsFragment
+    }
+    @Test
+    fun testItemCountIs10() {
+        val actualResult = activity?.news_list?.adapter?.itemCount
+        Assert.assertEquals(11,actualResult)
+    }
+//    @Test
+//    fun testSnackBarIsMade() {
+//        val actualResult = activity?.news_list?.layoutManager
+//        Assert.assertEquals(11,actualResult)
+//    }
+//    @Test
+//    fun testNewsManagerNotNull() {
+//        ReflectionHelpers.callInstanceMethod<Unit>(newsFragment, "requestNews")
+//        val test = ReflectionHelpers.getField<Lazy<NewsManager>>(newsFragment, "newsManager")
+//        val actualResult = test.value
+//        Assert.assertNotNull(test)
+//    }
+}
