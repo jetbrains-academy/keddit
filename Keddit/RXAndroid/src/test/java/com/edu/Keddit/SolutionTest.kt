673b0bf214a243583ca6a27b486a943d9efa56ce
Geravant <igor.gera.gerasim@gmail.com>
2018-11-01 16:53:03 +0300

    Task 24 (RXAndroid) instrumental tests are implemented

---
+++ Keddit/RXAndroid/src/test/java/com/edu/Keddit/SolutionTest.kt
@@ -0,0 +1,11 @@
+package com.edu.Keddit
+
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+
+
+@RunWith(RobolectricTestRunner::class)
+class SolutionTest {
+    //The instrumental tests are implemented
+}
