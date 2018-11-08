40cdb9e209d03f07fddf3a59b007d542441484e3
Geravant <igor.gera.gerasim@gmail.com>
2018-11-08 12:44:03 +0300

    task33 (Infinite scroll) tests implemented.

---
+++ Keddit/Infinite Scroll/src/test/java/com/edu/Keddit/SolutionTest.kt
@@ -0,0 +1,46 @@
+package com.edu.Keddit
+
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.edu.Keddit.commons.InfiniteScrollListener
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import kotlinx.android.synthetic.main.news_fragment.*
+import java.lang.IllegalArgumentException
+import java.lang.reflect.Array
+
+@RunWith(RobolectricTestRunner::class)
+class SolutionTest {
+    private var activity: MainActivity? = null
+    private var infiniteScrollListener: InfiniteScrollListener? = null
+    @Before
+    fun setup() {
+        activity = Robolectric.buildActivity(MainActivity::class.java).create().visible().start().get()
+
+    }
+    @Test
+    fun testInfiniteScrollListenerParameters() {
+        var error: String? = null
+        try {
+            infiniteScrollListener = InfiniteScrollListener({test()}, LinearLayoutManager(activity?.baseContext) )
+        }
+        catch (e: IllegalArgumentException){
+            error = e.message
+        }
+        Assert.assertNull("Check what arguments does the constructor of InfiniteScrollListener take.",error)
+    }
+    @Test
+    fun testNewsNotNull() {
+        val actualResult = activity?.news_list
+        var listenerField = RecyclerView::class.java.getDeclaredField("mScrollListeners")
+        listenerField.isAccessible = true
+        val actualListener = (listenerField.get(actualResult) as ArrayList<*>)[0]
+        Assert.assertTrue("Check if ",InfiniteScrollListener::class.java.isInstance(actualListener))
+    }
+
+    private fun test() {    }
+}
