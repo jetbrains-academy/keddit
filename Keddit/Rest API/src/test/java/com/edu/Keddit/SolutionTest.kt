7e0f810458b77717c7d27e42c1545a58b1bfc59a
Geravant <igor.gera.gerasim@gmail.com>
2018-11-12 22:14:41 +0300

    Tests restored

--- Keddit/Rest API/src/test/java/com/edu/Keddit/SolutionTest.kt
+++ Keddit/Rest API/src/test/java/com/edu/Keddit/SolutionTest.kt
@@ -1,14 +1,31 @@
 package com.edu.Keddit
 
+import com.edu.Keddit.api.RedditApi
+import com.edu.Keddit.api.RestAPI
 import kotlinx.android.synthetic.main.activity_main.view.*
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
-class ExampleUnitTest {
+class SolutionTest {
+    private var restApi: RestAPI? = null
+
+    @Before
+    fun setUp() {
+        restApi = RestAPI()
+    }
     @Test
-    fun testSolution() {
-        //TODO: implement your test here
-        val m  = MainActivity()
-        Assert.assertTrue("Tests not implemented for the task", true)
+    fun testInit() {
+        var actualResult = RestAPI::class.java.getDeclaredField("redditApi")
+        actualResult.isAccessible = true
+        var actualField : RedditApi? = null
+        try{
+            actualField = actualResult.get(restApi) as RedditApi}
+        catch (e: NoSuchFieldException) {
+            actualField = null
+        }
+        Assert.assertTrue(RedditApi::class.java.isInstance(actualField))
     }
 }
