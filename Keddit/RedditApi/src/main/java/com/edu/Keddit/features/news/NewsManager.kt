dc184f76bb3c7af2723285007fce035ebdcde7ec
Geravant <igor.gera.gerasim@gmail.com>
2019-01-29 10:26:45 +0300

    Task 26 (RedditAPI) task description, semicolons, watermark

--- Keddit/RedditApi/src/main/java/com/edu/Keddit/features/news/NewsManager.kt
+++ Keddit/RedditApi/src/main/java/com/edu/Keddit/features/news/NewsManager.kt
@@ -3,12 +3,7 @@ package com.edu.Keddit.features.news
 import com.edu.Keddit.commons.RedditNewsItem
 import rx.Observable
 
-/**
- * News Manager allows you to request more news from Reddit.
- *
- * @author juancho
- */
-class NewsManager() {
+class NewsManager {
 
     fun getNews(): Observable<List<RedditNewsItem>> {
         return Observable.create {
