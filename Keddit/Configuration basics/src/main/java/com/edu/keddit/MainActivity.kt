1152c7bd3162a9887bf1573b86226156ce4d851e
Geravant <igor.gera.gerasim@gmail.com>
2019-10-07 12:36:13 +0300

    Configuration basics - placeholders added, test mock implemented

--- Keddit/Configuration basics/src/main/java/com/edu/keddit/MainActivity.kt
+++ Keddit/Configuration basics/src/main/java/com/edu/keddit/MainActivity.kt
@@ -3,7 +3,7 @@ package com.edu.keddit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
--- Keddit/Configuration basics/src/test/java/com/edu/keddit/SolutionTest.kt
+++ Keddit/Configuration basics/src/test/java/com/edu/keddit/SolutionTest.kt
@@ -2,10 +2,22 @@ package com.edu.keddit
 
 import org.junit.Assert
 import org.junit.Test
+import java.lang.Error
 
 class SolutionTest {
     @Test
     fun testSolution() {
-        Assert.assertTrue("Congratulations! You can move on to the next task!", true)
+        //Arrange
+        var actual_error: Error? = null
+        //Act
+        //TODO: implement tests
+        try {
+            val actual  = MainActivity()
+        }
+        catch (e: Error) {
+            actual_error = e
+        }
+        //Assert
+        Assert.assertNull("", actual_error)
     }
 }
