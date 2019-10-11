c09e5b5f32ba763ea72e91e87c00432a6324cc9c
Geravant <igor.gera.gerasim@gmail.com>
2019-10-11 17:04:56 +0300

    feat(New UI tasks): The functionality for the new UI tasks is allmost there
    New task added, including the new activity, navGraph and a button to
    navigate to the activity. The activity should be changed to a fragment
    and the NavGraph should be fixed.
    
    BREAKING CHANGE: Main Activity in the more on activities task would not
    install because of the incorrect navGrapgh.

---
+++ Keddit/More on Activities/src/main/java/com/edu/keddit/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.edu.keddit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    val toolbar = findViewById(R.id.toolbar) as Toolbar
+    setSupportActionBar(toolbar)
+  }
+
+  fun clearBackStack() {
+    val manager = supportFragmentManager
+    if (manager.backStackEntryCount > 0) {
+      val first = manager.getBackStackEntryAt(0)
+      manager.popBackStack(first.id, androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE)
+    }
+  }
+}
