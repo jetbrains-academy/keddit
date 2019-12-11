dfa9c0ed9b8bf81f0bc6a919c79c0797ceb11b7a
Geravant <igor.gera.gerasim@gmail.com>
2019-12-11 15:44:12 +0300

    refactor(Adding Another fragment ): view inflate code changed to a modern version
    ..And some tests are added

--- Keddit/Safe/src/test/java/com/edu/keddit/SolutionTest.kt
+++ Keddit/Safe/src/test/java/com/edu/keddit/SolutionTest.kt
@@ -1,12 +1,39 @@
 package com.edu.keddit
 
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class SolutionTest {
+    private var activity: MainActivity? = null
+    private var manager: androidx.fragment.app.FragmentManager? = null
+    @Before
+    fun setup() {
+        activity = Robolectric.buildActivity(MainActivity::class.java).create().get()
+        manager = activity?.supportFragmentManager
+
+    }
+
     @Test
-    fun testSolution() {
-        val m  = MainActivity()
-        Assert.assertTrue("No need in test", true)
+    fun testManagerIsDeclaredInClearBackStack() {
+        val ft = manager?.beginTransaction()
+        ft?.addToBackStack(null)
+        ft?.commit()
+        val listener = TestListener()
+        manager?.addOnBackStackChangedListener(listener)
+        activity?.clearBackStack()
+        Assert.assertTrue("Fragment is not set, check your clearBackStack() method.", listener.modificationCount>0)
+    }
+}
+
+
+class TestListener: androidx.fragment.app.FragmentManager.OnBackStackChangedListener {
+    var modificationCount = 0
+    override fun onBackStackChanged () {
+        modificationCount++
     }
 }
--- Keddit/Statically Typed/src/test/java/com/edu/keddit/SolutionTest.kt
+++ Keddit/Statically Typed/src/test/java/com/edu/keddit/SolutionTest.kt
@@ -1,12 +1,30 @@
 package com.edu.keddit
 
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class SolutionTest {
+    private var activity: MainActivity? = null
+    @Before
+    fun setup() {
+        activity = Robolectric.buildActivity(MainActivity::class.java).create().get()
+        // Filling up the backStack with smth
+        val ft = activity?.supportFragmentManager?.beginTransaction()
+        ft?.addToBackStack("test")
+        ft?.commit()
+    }
+
+
     @Test
-    fun testSolution() {
-        val m  = MainActivity()
-        Assert.assertTrue("Congratulations! You can move on to the next task.", true)
+    fun testClearBackStackIsCalledIfCleanStackTrue() {
+        val exprectedBackStack = activity?.supportFragmentManager?.getBackStackEntryAt(0)
+        activity?.changeFragment(androidx.fragment.app.Fragment(), true)
+        val actualBackStack = activity?.supportFragmentManager?.getBackStackEntryAt(0)
+        Assert.assertNotEquals("You should not change the cleanStack if it is no null - check you do not reassign it anywhere",exprectedBackStack, actualBackStack)
     }
 }
