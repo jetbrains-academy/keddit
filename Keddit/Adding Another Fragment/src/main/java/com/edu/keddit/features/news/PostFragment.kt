7d4260b215570fb28d2afcfe61face1bf05811c1
Geravant <igor.gera.gerasim@gmail.com>
2019-12-17 17:57:18 +0300

    Dependencies in config tasks added for tasks Adding Another Fragment and above

--- Keddit/Adding Another Fragment/src/main/java/com/edu/keddit/features/news/PostFragment.kt
+++ Keddit/Adding Another Fragment/src/main/java/com/edu/keddit/features/news/PostFragment.kt
@@ -7,15 +7,12 @@ import android.view.ViewGroup
 import com.edu.keddit.R
 import android.webkit.WebViewClient
 import android.webkit.WebView
-
-
+import com.edu.keddit.commons.inflate
 
 
 class PostFragment : androidx.fragment.app.Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.post_fragment, container, false)
-
-        return view
+        return container?.inflate(R.layout.post_fragment)
     }
 }
