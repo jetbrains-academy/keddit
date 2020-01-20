3e4a03d69ed4809231c42cb991f9f19d972475c5
Geravant <igor.gera.gerasim@gmail.com>
2020-01-20 12:15:34 +0300

    fix(Properties and fields): Import fixed

--- Keddit/Properties and Fields/src/main/java/com/edu/keddit/MainActivity.kt
+++ Keddit/Properties and Fields/src/main/java/com/edu/keddit/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     val manager = supportFragmentManager
     if (manager.backStackEntryCount > 0) {
       val first = manager.getBackStackEntryAt(0)
-      manager.popBackStack(first.id, androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE)
+      manager.popBackStack(first.id, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
   }
 
